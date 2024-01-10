a32042f8f37cee7f9a517d8c4cfcf6aab29a1304
arshadmannantavida <arshad.mannantavida@adapty.com>
2024-01-10 10:25:04 +0530

    PF-281

---
+++ Simpl/Checkout/Model/Adminhtml/Source/Mode.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Simpl\Checkout\Model\Adminhtml\Source;
+
+class Mode
+{
+    const TEST = 'test';
+    const LIVE = 'live';
+
+    public function toOptionArray()
+    {
+        return [
+            [
+                'value' => self::TEST,
+                'label' => __('Test')
+            ],
+            [
+                'value' => self::LIVE,
+                'label' => __('Live')
+            ],
+        ];
+    }
+}
