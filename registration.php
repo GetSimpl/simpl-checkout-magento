f762ec2c143e328493cdbe8ed297057d5653e1b4
arshadmannantavida <arshad.mannantavida@adapty.com>
2024-01-11 13:21:27 +0530

    Config files added

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Simpl Checkout All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Simpl_Checkout', __DIR__);
+
