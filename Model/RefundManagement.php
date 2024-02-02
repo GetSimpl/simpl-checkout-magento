c96b8e4b35b4d4c3436a2b64c1077c742703ae08
arshadmannantavida <arshad.mannantavida@adapty.com>
2024-02-02 17:53:39 +0530

    PF-280 RefundConfirmResponse class added

--- Model/RefundManagement.php
+++ Model/RefundManagement.php
@@ -4,20 +4,15 @@ namespace Simpl\Checkout\Model;
 
 use Magento\Sales\Api\Data\CreditmemoCommentInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
-use Magento\Sales\Model\Order;
 use Magento\Sales\Model\Order\Creditmemo;
 use Magento\Sales\Model\Order\CreditmemoRepository;
-use Simpl\Checkout\Api\Data\ApiDataInterface;
-use Simpl\Checkout\Api\Data\ErrorDataInterface;
-use Simpl\Checkout\Api\Data\MessageDataInterface;
+use Simpl\Checkout\Model\Data\RefundConfirmResponse;
 use Simpl\Checkout\Api\RefundManagementInterface;
 use Simpl\Checkout\Helper\SimplApi;
 
 class RefundManagement implements RefundManagementInterface {
 
-    protected $apiResponseData;
-
-    protected $messageData;
+    protected $refundConfirmResponse;
 
     protected $orderRepository;
 
@@ -25,34 +20,25 @@ class RefundManagement implements RefundManagementInterface {
 
     protected $creditmemoComment;
 
-    protected $errorData;
-
     protected $simplApi;
 
     /**
-     * @param ApiDataInterface $apiResponseData
-     * @param MessageDataInterface $messageData
-     * @param ErrorDataInterface $errorData
      * @param OrderRepositoryInterface $orderRepository
      * @param CreditmemoRepository $creditmemoRepository
      * @param CreditmemoCommentInterface $creditmemoComment
      * @param SimplApi $simplApi
      */
     public function __construct(
-        ApiDataInterface    $apiResponseData,
-        MessageDataInterface    $messageData,
-        ErrorDataInterface  $errorData,
+        RefundConfirmResponse $refundConfirmResponse,
         OrderRepositoryInterface    $orderRepository,
         CreditmemoRepository    $creditmemoRepository,
         CreditmemoCommentInterface  $creditmemoComment,
         SimplApi    $simplApi
     ) {
-        $this->apiResponseData = $apiResponseData;
-        $this->messageData = $messageData;
+        $this->refundConfirmResponse = $refundConfirmResponse;
         $this->orderRepository = $orderRepository;
         $this->creditmemoRepository = $creditmemoRepository;
         $this->creditmemoComment = $creditmemoComment;
-        $this->errorData = $errorData;
         $this->simplApi = $simplApi;
     }
 
@@ -61,20 +47,15 @@ class RefundManagement implements RefundManagementInterface {
      */
     public function confirm($creditMemoId, $orderId, $transactionId, $status) {
 
-        $this->apiResponseData->setSuccess(false);
-
         try {
             $creditMemo =  $this->creditmemoRepository->get($creditMemoId);
 
             if ($creditMemo->getOrderId() != $orderId) {
-                throw new \Exception('invalid access');
+                throw new \Exception('invalid credit memo');
             }
 
             if (!$this->simplApi->validateRefund($creditMemoId, $orderId, $transactionId, $status)) {
-                $this->errorData->setCode('validation_failed');
-                $this->errorData->setMessage('Validation failed');
-                $this->apiResponseData->setError($this->errorData);
-                return $this->apiResponseData;
+                return $this->refundConfirmResponse->setError('validation_failed', 'Validation failed');
             }
 
             if ($status == "REFUND_SUCCESS") {
@@ -92,18 +73,11 @@ class RefundManagement implements RefundManagementInterface {
             $creditMemo->setComments([$this->creditmemoComment]);
             $this->creditmemoRepository->save($creditMemo);
 
-            $this->apiResponseData->setSuccess(true);
-            $this->messageData->setMessage("refund confirm successfully");
-
         } catch (\Exception $e) {
-            $this->errorData->setCode('error_request');
-            $this->errorData->setMessage($e->getMessage());
-            $this->apiResponseData->setError($this->errorData);
-            return $this->apiResponseData;
+
+            return $this->refundConfirmResponse->setError('error_request', $e->getMessage());
         }
 
-        $this->apiResponseData->setData($this->messageData);
-
-        return $this->apiResponseData;
+        return $this->refundConfirmResponse->setMessage("refund confirm successfully");
     }
 }
