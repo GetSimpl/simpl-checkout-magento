38ac7d21e1c9abb7325d556b9d66f9aeaa331a7a
arshadmagento <52126156+arshadmagento@users.noreply.github.com>
2024-02-05 16:16:35 +0530

    PF-279 Change as part of PF-277 changes (#19)
    * PF-279 Change as part of PF-277 changes
    
    * PF-279 change request
    
    * renamed OrderDetails to GetOrderDetails
    
    ---------
    
    Co-authored-by: arshadmannantavida <arshad.mannantavida@adapty.com>

---
+++ Model/Data/Order/GetOrderResponse.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Simpl\Checkout\Model\Data\Order;
+
+use Simpl\Checkout\Api\Data\ErrorDataInterface;
+use Simpl\Checkout\Api\Data\OrderDataInterface;
+
+class GetOrderResponse
+{
+    /**
+     * @var ErrorDataInterface
+     */
+    protected $errorData;
+
+    /**
+     * @var OrderDataInterface
+     */
+    protected $orderData;
+
+    /**
+     * @param ErrorDataInterface $errorData
+     * @param OrderDataInterface $orderData
+     */
+    public function __construct(
+        ErrorDataInterface $errorData,
+        OrderDataInterface $orderData
+    ) {
+        $this->errorData = $errorData;
+        $this->orderData = $orderData;
+    }
+
+    /**
+     * @param $order
+     * @return OrderDataInterface
+     */
+    public function setOrder($order) {
+
+        $this->orderData->setData($order);
+        $this->orderData->setSuccess(true);
+        return $this->orderData;
+    }
+
+    /**
+     * @return OrderDataInterface
+     */
+    public function orderNotFoundError() {
+
+        $this->orderData->setSuccess(false);
+        $this->errorData->setCode("order_not_found");
+        $this->errorData->setMessage("Order not found");
+        $this->orderData->setError($this->errorData);
+        return $this->orderData;
+    }
+}
