ea5e585c1eb3db4a7ec0c98e5542d3fb37ca357e
arshadmagento <52126156+arshadmagento@users.noreply.github.com>
2024-02-05 14:56:25 +0530

    PF-328[API] Order response payload cleanup (#17)
    Co-authored-by: arshadmannantavida <arshad.mannantavida@adapty.com>

--- Model/Simpl/Order.php
+++ Model/Simpl/Order.php
@@ -53,108 +53,37 @@ class Order {
      * @return array
      */
     public function getCurrentOrder() {
-        $orderId = $this->checkoutSession->getLastRealOrder();
-        return $this->getOrder($orderId);
+        $order = $this->checkoutSession->getLastRealOrder();
+        return $this->getOrder($order);
     }
 
 
     /**
      * To get order data as per the payment initiate api contract
-     * @param $id
+     * @param \Magento\Sales\Model\Order $order
      * @return array|null
      * @throws \Exception
      */
-    public function getOrder($id) {
-        $data = null;
-        try {
-            $order = $this->orderRepository->get($id);
-        }catch (\Exception $e) {
-            throw new \Exception('Error getting order');
-        }
-        $data["order_id"] = $id;
+    public function getOrder($order) {
+        $data = $order->getData();
         $data["checkout_url"] = $this->url->getCurrentUrl();
-        $data["increment_id"] = $order->getIncrementId();
-        $data["customer"] = [
-            "firstname" => $order->getCustomerFirstname(),
-            "lastname" => $order->getCustomerLastname(),
-            "mobile" => $order->getBillingAddress()->getTelephone(),
-            "email" => $order->getCustomerEmail()
-        ];
         $data["shipping_address"] = $order->getShippingAddress()->getData();
         $data["billing_address"] = $order->getBillingAddress()->getData();
 
-        $data["totals"] = $this->getTotals($order);
-        $data["total_segments"] = $this->getTotalsSegments($order);
+        $orderItems = $order->getAllItems();
+        $items = [];
+        foreach ($orderItems as $orderItem) {
+            $orderItemArray = $orderItem->getData();
+            $items[] = $orderItemArray;
+        }
 
-        $data["shipping_method_code"] = $order->getShippingMethod();
-        $data["shipping_carrier_code"] = $order->getShippingMethod();
+        $data["items"] = $items;
 
         $data["figerprint"] = [
             "user_ip" => $order->getRemoteIp(),
             "user_agent" => $_SERVER['HTTP_USER_AGENT']
         ];
 
-        return $data;
-    }
-
-    /**
-     * @param \Magento\Sales\Api\Data\OrderInterface $order
-     * @return array
-     */
-    private function getTotalsSegments($order) {
-
-        $totals = [];
-        $orderNew = $this->order->create()->loadByIncrementId($order->getIncrementId());
-        $totalsSegments = $orderNew->getTotals();
-
-        if ($totalsSegments) {
-            foreach ($totalsSegments as $totalSegment) {
-                $totals[]['code'] = $totalSegment->getCode();
-                $totals[]['title'] = $totalSegment->getTitle();
-                $totals[]['value'] = $totalSegment->getValue();
-            }
-        }
-
-        return $totals;
-    }
-
-    /**
-     * @param \Magento\Sales\Api\Data\OrderInterface $order
-     * @return array
-     */
-    private function getTotals($order) {
-
-        $orderItems = $order->getAllItems();
-        $items = [];
-
-        foreach ($orderItems as $orderItem) {
-
-            $orderItemArray = $orderItem->getData();
-            $items[] = $orderItemArray;
-        }
-
-        $data = [
-            "grand_total" => $order->getGrandTotal(),
-            "base_grand_total" => $order->getBaseGrandTotal(),
-            "subtotal" => $order->getSubtotal(),
-            "base_subtotal" => $order->getBaseSubtotal(),
-            "discount_amount" => $order->getDiscountAmount(),
-            "base_discount_amount" => $order->getBaseDiscountAmount(),
-            "shipping_amount" => $order->getShippingAmount(),
-            "base_shipping_amount" => $order->getBaseShippingAmount(),
-            "shipping_discount_amount" => $order->getShippingDiscountAmount(),
-            "base_shipping_discount_amount" => $order->getBaseShippingDiscountAmount(),
-            "tax_amount" => $order->getTaxAmount(),
-            "base_tax_amount" => $order->getBaseTaxAmount(),
-            "shipping_tax_amount" => $order->getShippingTaxAmount(),
-            "base_shipping_tax_amount" => $order->getBaseShippingTaxAmount(),
-            "subtotal_incl_tax" => $order->getSubtotalInclTax(),
-            "shipping_incl_tax" => $order->getShippingInclTax(),
-            "base_shipping_incl_tax" => $order->getBaseShippingInclTax(),
-            "base_currency_code" => $order->getBaseCurrencyCode(),
-            "items_qty" => $order->getTotalQtyOrdered(),
-            "items" => $items
-        ];
         return $data;
     }
 }
