54aab458f5bda2b57100de31d9ae73711ba92a73
arshadmannantavida <arshad.mannantavida@adapty.com>
2024-02-06 12:21:33 +0530

    PF-278 Order update

---
+++ Api/OrderUpdateManagementInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Simpl\Checkout\Api;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Simpl\Checkout\Api\Data\Order\PaymentDataInterface;
+use Simpl\Checkout\Api\Data\Order\TransactionDataInterface;
+use Simpl\Checkout\Api\Data\ApiDataInterface;
+
+interface OrderUpdateManagementInterface
+{
+    /**
+     * Confirm Simpl Checkout Order by ID
+     * @param string $orderId
+     * @param PaymentDataInterface $payment
+     * @param TransactionDataInterface $transaction
+     * @return ApiDataInterface
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     */
+    public function update($orderId, $payment, $transaction);
+}
---
+++ Model/Data/Order/OrderUpdateResponse.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Simpl\Checkout\Model\Data\Order;
+
+use Simpl\Checkout\Api\Data\Order\RedirectionUrlDataInterface;
+use Simpl\Checkout\Api\Data\Order\OrderConfirmSuccessDataInterface;
+use Simpl\Checkout\Api\Data\ErrorDataInterface;
+use Simpl\Checkout\Api\Data\ApiDataInterface;
+use Simpl\Checkout\Api\Data\MessageDataInterface;
+
+class OrderUpdateResponse
+{
+    /**
+     * @var ApiDataInterface
+     */
+    protected $apiData;
+    /**
+     * @var ErrorDataInterface
+     */
+    protected $errorData;
+
+    /**
+     * @var MessageDataInterface
+     */
+    protected $messageData;
+
+    /**
+     * @var RedirectionUrlDataInterface
+     */
+    protected $redirectionUrlData;
+
+    /**
+     * @var OrderConfirmSuccessDataInterface
+     */
+    protected $orderConfirmSuccessData;
+
+    /**
+     * @param ApiDataInterface $apiData
+     * @param ErrorDataInterface $errorData
+     * @param MessageDataInterface $messageData
+     * @param RedirectionUrlDataInterface $redirectionUrlData
+     * @param OrderConfirmSuccessDataInterface $orderConfirmSuccessData
+     */
+    public function __construct(
+        ApiDataInterface $apiData,
+        ErrorDataInterface $errorData,
+        MessageDataInterface $messageData,
+        RedirectionUrlDataInterface $redirectionUrlData,
+        OrderConfirmSuccessDataInterface $orderConfirmSuccessData
+    ) {
+        $this->apiData = $apiData;
+        $this->errorData = $errorData;
+        $this->messageData = $messageData;
+        $this->redirectionUrlData = $redirectionUrlData;
+        $this->orderConfirmSuccessData = $orderConfirmSuccessData;
+    }
+
+    /**
+     * @param $code
+     * @param $message
+     * @return OrderConfirmSuccessDataInterface
+     */
+    public function setError($code, $message) {
+
+        $this->orderConfirmSuccessData->setSuccess(false);
+        $this->errorData->setCode($code);
+        $this->errorData->setMessage($message);
+        $this->orderConfirmSuccessData->setError($this->errorData);
+        return $this->orderConfirmSuccessData;
+    }
+
+    /**
+     * @param $message
+     * @return ApiDataInterface
+     */
+    public function setMessage($message) {
+
+        $this->apiData->setSuccess(true);
+        $this->messageData->setMessage($message);
+        $this->apiData->setData($this->messageData);
+        return $this->apiData;
+    }
+}
