9918fdd6e24378b63512fbcbe9b93a4412955a1b
arshadmannantavida <arshad.mannantavida@adapty.com>
2024-02-07 11:15:51 +0530

    PF-300 Back button handling

---
+++ Controller/Payment/Restore.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Simpl\Checkout\Controller\Payment;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Framework\Controller\Result\JsonFactory;
+
+
+class Restore implements HttpGetActionInterface
+{
+
+    protected $checkoutSession;
+
+    protected $resultRedirectFactory;
+
+    protected $orderRepository;
+
+    protected $manager;
+
+    protected $quoteRepository;
+
+    protected $jsonFactory;
+
+    public function __construct(
+        CheckoutSession $checkoutSession,
+        ManagerInterface $manager,
+        OrderRepositoryInterface $orderRepository,
+        RedirectFactory $resultRedirectFactory,
+        CartRepositoryInterface $quoteRepository,
+        JsonFactory $jsonFactory
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->manager = $manager;
+        $this->orderRepository = $orderRepository;
+        $this->resultRedirectFactory = $resultRedirectFactory;
+        $this->quoteRepository = $quoteRepository;
+        $this->jsonFactory = $jsonFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute() {
+
+        try {
+
+            // Cancel the last order
+            $order = $this->checkoutSession->getLastRealOrder();
+            $order->setState(Order::STATE_CANCELED);
+            $order->setStatus(Order::STATE_CANCELED);
+            $this->orderRepository->save($order);
+
+            // Restore the quote
+            $quote = $this->quoteRepository->get($order->getQuoteId());
+            $quote->setIsActive(true);
+            $this->quoteRepository->save($quote);
+            $this->checkoutSession->replaceQuote($quote);
+
+            $data = [
+                'status' => 'success',
+                'url' => 'checkout/#payment'
+            ];
+
+        } catch (\Exception $e) {
+
+            $data = [
+                'status' => 'success',
+                'url' => 'checkout/cart',
+                'message' => $e->getMessage()
+            ];
+        }
+
+        $resultJson = $this->jsonFactory->create();
+        $resultJson->setData($data);
+        return $resultJson;
+    }
+}
--- view/frontend/web/js/view/payment/method-renderer/simplcheckout-method.js
+++ view/frontend/web/js/view/payment/method-renderer/simplcheckout-method.js
@@ -3,15 +3,23 @@ define(
         'Magento_Checkout/js/view/payment/default',
         'Magento_Checkout/js/action/redirect-on-success',
         'mage/url',
-        'mage/storage'
+        'mage/storage',
+        'Magento_Checkout/js/model/payment/additional-validators'
     ],
-    function (Component, redirectOnSuccessAction, url, storage) {
+    function (
+        Component,
+        redirectOnSuccessAction,
+        url,
+        storage,
+        additionalValidators
+    ) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'Simpl_Checkout/payment/simplcheckout'
             },
             redirectAfterPlaceOrder: false,
+            orderId: null,
             getInstructions: function () {
                 return window.checkoutConfig.payment.simplcheckout.instructions;
             },
@@ -19,20 +27,83 @@ define(
                 return window.checkoutConfig.payment.simplcheckout.title_for_frontend;
             },
             /**
+             * Place order.
+             */
+            placeOrder: function (data, event) {
+                var self = this;
+
+                if (event) {
+                    event.preventDefault();
+                }
+
+                if (!self.orderId) {
+                    if (this.validate() &&
+                        additionalValidators.validate() &&
+                        this.isPlaceOrderActionAllowed() === true
+                    ) {
+                        this.isPlaceOrderActionAllowed(false);
+
+                        this.getPlaceOrderDeferredObject()
+                            .done(
+                                function (orderId) {
+                                    self.orderId =  orderId;
+                                    self.afterPlaceOrder();
+                                    self.initSimplCheckout();
+                                }
+                            ).always(
+                            function () {
+                                self.isPlaceOrderActionAllowed(true);
+                            }
+                        );
+
+                        return true;
+                    }
+                } else {
+                    self.initSimplCheckout();
+                }
+
+                return false;
+            },
+            /**
              * After place order callback
              */
-            afterPlaceOrder: function () {
+            initSimplCheckout: function () {
+                var self = this;
                 var paymentInit = url.build('simpl/payment/init');
                 storage.get(
                     paymentInit
                 ).fail(
                     function (response) {
-                        //TODO TO HANDLE IF PAYMENT INIT REQUEST FAIL
+                        self.restoreCart();
                     }
                 ).done(
                     function (response) {
                         if (response.status !== 'error') {
                             window.location.replace(response.url);
+                        } else {
+                            self.restoreCart();
+                        }
+                    }
+                );
+            },
+            /**
+             * Function to restore cart if payment failed.
+             */
+            restoreCart: function () {
+                var restoreCartUrl = url.build('simpl/payment/restore');
+                var cartUrl = url.build('checkout/cart/');
+                storage.get(
+                    restoreCartUrl
+                ).fail(
+                    function (response) {
+                        window.location.replace(cartUrl);
+                    }
+                ).done(
+                    function (response) {
+                        if (response.status) {
+                            window.location.replace(url.build(response.url));
+                        } else {
+                            window.location.replace(cartUrl);
                         }
                     }
                 );
