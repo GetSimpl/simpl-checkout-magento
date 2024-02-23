bdeaab697e2311af143044877f6efba6be44070a
arshadmannantavida <arshad.mannantavida@adapty.com>
2024-02-23 16:38:34 +0530

    Logger

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -2,6 +2,42 @@
 
 namespace Simpl\Checkout\Logger;
 
+use DateTimeZone;
+use Simpl\Checkout\Helper\Config;
+
 class Logger extends \Monolog\Logger
 {
+    protected $config;
+
+    public function __construct(
+        Config $config,
+        string $name,
+        array $handlers = [],
+        array $processors = [],
+        ?DateTimeZone $timezone = null
+    ) {
+        $this->config = $config;
+        parent::__construct($name, $handlers, $processors, $timezone);
+    }
+
+    public function info($message, array $context = []): void
+    {
+        if ($this->config->isLogEnabled()) {
+            parent::info($message, $context);
+        }
+    }
+
+    public function error($message, array $context = []): void
+    {
+        if ($this->config->isLogEnabled()) {
+            parent::error($message, $context);
+        }
+    }
+
+    public function critical($message, array $context = []): void
+    {
+        if ($this->config->isLogEnabled()) {
+            parent::critical($message, $context);
+        }
+    }
 }
