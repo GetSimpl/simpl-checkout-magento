eab6acaa7ebedda1c1376bf9d3b067064d46e445
arshadmannantavida <arshad.mannantavida@adapty.com>
2024-03-14 09:17:22 +0530

    Update as per comment

--- Model/Total/Invoice/SimplAdditionalFields.php
+++ Model/Total/Invoice/SimplAdditionalFields.php
@@ -3,21 +3,25 @@ declare(strict_types = 1);
 
 namespace Simpl\Checkout\Model\Total\Invoice;
 
-class SimplAdditionalFields extends \Magento\Sales\Model\Order\Invoice\Total\AbstractTotal
+use Simpl\Checkout\Model\Total\SimplAdditionalFields as SimplFields;
+use Magento\Sales\Model\Order\Invoice;
+use Magento\Sales\Model\Order\Invoice\Total\AbstractTotal;
+
+class SimplAdditionalFields extends AbstractTotal
 {
     /**
      * @inheritdoc
      */
-    public function collect(\Magento\Sales\Model\Order\Invoice $invoice)
+    public function collect(Invoice $invoice)
     {
         parent::collect($invoice);
 
         if ($invoice->getOrder()->getExtensionAttributes()) {
             $simplDiscounts = (float)$invoice->getOrder()->getExtensionAttributes()->getSimplAppliedDiscounts();
-            $invoice->setData(\Simpl\Checkout\Model\Total\SimplAdditionalFields::SIMPL_DISCOUNT, $simplDiscounts);
+            $invoice->setData(SimplFields::SIMPL_DISCOUNT, $simplDiscounts);
 
             $simplCharges = (float)$invoice->getOrder()->getExtensionAttributes()->getSimplAppliedCharges();
-            $invoice->setData(\Simpl\Checkout\Model\Total\SimplAdditionalFields::SIMPL_CHARGES, $simplCharges);
+            $invoice->setData(SimplFields::SIMPL_CHARGES, $simplCharges);
         }
 
         return $this;
