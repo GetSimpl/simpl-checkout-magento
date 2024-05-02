a33a56ea855110c8158bcead22ac222eeaae224c
pranavraaghavsimpl <pranav.raaghav@getsimpl.com>
2024-05-02 16:24:32 +0530

    fix: add semi-colon

--- Helper/Config.php
+++ Helper/Config.php
@@ -27,7 +27,7 @@ class Config extends AbstractHelper
     const KEY_PAYMENT_INS = 'instructions';
     const KEY_SIMPL_LIVE_HOST_URL = 'https://checkout-platform-integrations.getsimpl.com/';
     const KEY_SIMPL_TEST_HOST_URL = 'https://sandbox-checkout-platform-integrations.getsimpl.com/';
-    const KEY_GENERATE_INVOICE = 'generate_invoice'
+    const KEY_GENERATE_INVOICE = 'generate_invoice';
 
     const ABANDONED_ORDER_LIFE_TIME = 60;
 
@@ -79,7 +79,7 @@ class Config extends AbstractHelper
     }
 
     public function isInvoiceGenerationEnabled() {
-        return $this->getSimplConfig(self::KEY_GENERATE_INVOICE)
+        return $this->getSimplConfig(self::KEY_GENERATE_INVOICE);
     }
 
     /**
